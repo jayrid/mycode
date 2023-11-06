136688342ec81f00d471d2b6b5813fcb6ff9a96e
jayrid <jessemridley@gmail.com>
2023-11-06 19:18:07 +0000

    reason for commit

---
+++ bestpractice.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""A multi-line comment to describe the script,
+    made with triple quotes"""
+
+def main(): # all code should appear within a function 
+    """all functions have multiline comments to describe them"""
+
+    my_string = "your code would go here" # vars use _ not camelCase
+    print(my_string)    # print to standard-out
+
+#  calling main() using this technique allows others to import your code
+if __name__=="__main__":
+    main()
